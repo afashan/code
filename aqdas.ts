0c6734b789f2383bdc9ea5b3b59722f2dc775c93
afshan <aqsadfatima@gmail.com>
2023-09-10 13:20:43 +0500

    code

---
+++ aqdas.ts
@@ -0,0 +1,287 @@
+/*var length : number=5;
+var breadth : number=7;
+var area : number=length*breadth;
+console.log("area")*/
+
+/*var length: number=8;
+var breadth : number=9;
+var height : number=3;
+var volume : number=length*height*breadth;
+console.log("volume")*/
+
+/*var givenNumber : number=5;
+if(givenNumber>5){
+    console.log("positive")
+}
+else{
+    console.log("negative")
+}*/
+
+/*var userage :  number=18;
+if(userage>18){
+    console.log("person is eligible for vote")
+}
+else{
+    console.log("person not elgible for vote")
+}*/
+
+/*var givenNumber : number=8;
+if("givenNumber/2"){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}*/
+
+/*number : var J=((10+7)*5-4/(9-8)*30+7);
+console.log("J")*/
+
+/*var celsius : number=200;
+var farenheit : number=100;
+var celsius : number=(farenheit-32)*5/9
+console.log("farenheit");
+var farenheit : number=(celsius*9/5)+32;
+console.log("celisus")*/
+
+/*var totalmarks : number=1100;
+var obtainedmarks : number=1084;
+var percentage : number= (obtainedmarks/totalmarks)*100;
+console.log("percentage" ,percentage)*/
+
+/*var days : number=17;
+var week : number=Math.floor(days/7);
+console.log("week");
+var remainingdays : number= (days%7);
+console.log("remainingdays")*/
+
+/*var productprice : number=300;
+if(productprice>100){
+    console.log("10% discount")
+}
+else{
+    console.log("5%discount")
+}*/
+ 
+/*var temperature : number=25;
+if(temperature<25){
+    console.log("you should wear warm clothes")
+}
+else{
+    console.log("wear normal clothes")
+}*/
+
+/*number : var  a=25;
+number : var b=a%3;
+if(b==0){
+    console.log("divisible by 3")
+}
+else{
+    console.log("divisible by 5")
+}*/
+
+/*number : var a=9;
+number : var b=a%3;
+if(b==0){
+    console.log("divisible by 3")
+}
+else{
+    console.log("divisible by 5")
+}*/
+ 
+/*var userage : number=6;
+if(userage<=12){
+    console.log("child")
+}
+else{
+    console.log("teenager")
+}
+var userage : number=16;
+if(userage<=19){
+    console.log("teenager")
+}
+else{
+    console.log("child")
+}
+var userage : number=16;
+if(userage>16){
+    console.log("adult")
+}
+else{
+    console.log("child")
+}*/ 
+
+
+
+/*var days : number=7;
+var daysname : string=("sunday");
+if(days=7){
+    console.log("sunday")
+}
+else if(days=1){
+    console.log("monday")
+}
+else{
+    console.log("not")
+}*/
+
+/*var days : number=28;
+if(days<28){
+    console.log("not a lunar year")
+}
+else if(days=28){
+    console.log("a lunar year")
+}
+else{
+    console.log("not")
+}*/
+
+/*var amountofbill : number=1000;
+if(amountofbill>100){
+    console.log("add 10% tax")
+}
+else if(amountofbill>200){
+    console.log("add 15% tax")
+}
+else{
+    console.log("add 25% tax")
+}*/
+
+ /*var fruits : string[]=["apple","mango","orange"]
+ fruits.pop()
+ console.log("updare array : ",fruits)*/
+
+ /*var fruits : string[]=["apple","orange","mango"]
+ fruits.push("grapes","banana")
+ console.log("update array : ",fruits)*/
+
+ /*var fruits : string[]=["apple","orange","peach"]
+ fruits.shift()
+ console.log("update array:",fruits)*/
+
+ /*var fruits : string[]=["apple","orange","mango","peach"]
+ fruits.unshift("strawberry","cherry")
+ console.log("update array:",fruits)*/
+
+ /*var fruits : string[]=["apple","mango","peach"]
+ fruits.splice(2,0,"cherry","orange")
+ console.log("update array:",fruits)*/
+
+ /*var rollNumber : number[]=[11,22,33,44]
+ rollNumber.pop()
+ console.log("update array :",rollNumber)*/
+ 
+ /*var rollNumber : number[]=[8,9,10,11]
+ rollNumber.push(1,2,3)
+ console.log("update array:",rollNumber)*/
+
+ /*var rollNumber : number[]=[22,33,44,55]
+ rollNumber.shift()
+ console.log("update array;",rollNumber)*/
+
+ /*var rollNumber : number[]=[1,2,3,4,5]
+ rollNumber.unshift(6,7,8)
+ console.log("update array :",rollNumber)*/
+
+ /*var rollNumber: number[]=[10,11,12,13]
+ rollNumber.splice(0,1,55,66)
+ console.log("update array :",rollNumber)*/
+
+//  number : var a=5;
+//  number :var i=0;
+//  for(i=1;i<=10;i++){
+//     console.log("a"+ "x" +i+ "=" +(a*i))
+//  }
+ /* type chairType = {company:string,price:number,color: string,isUsed : boolean,material:string};
+  
+ var  chair: chairType[]=[{
+    company : "Masters",
+    price : 2000,
+    color : "brown",
+    isUsed: true,
+    material : "wooden"
+ }]
+  console.log(chair[0].company);*/
+
+  /*string : var chairtype =1;
+  do{var company : string="masters";
+      var color : string ="black";
+      var result : string=color;
+     console.log(result);
+     chairtype++;
+  }while(chairtype<=10)*/
+
+  /*var givenNumber : number=5;
+  do{
+    number : var a=8;
+    number : var b=9;
+     number : var c=a+b;
+     console.log(c);
+     givenNumber++;
+  }while(givenNumber<=11);*/
+
+ /* type car={color:string,price:number,company:string,isusedcar:boolean};
+  var tool : car[]=[
+   {color: "red",price: 20000000 , company: "honda",isusedcar: true },
+   {color: "black",price: 12000000 , company: "suzuki",isusedcar: false}
+  ];
+  console.log(tool[1].color);
+
+  type carType ={color:string, company:string, price:number, isused:boolean, year:number}
+  var car : carType[]=[]  
+
+
+
+ /* console.log("before aysnc code");
+
+  function dosomething(){
+    console.log("print something");
+  }
+  setTimeout(dosomething, 100000)
+
+  setInterval(dosomething,1000)
+
+
+  console.log("after async code ");
+
+  /*var car{}={
+    price: 3000000,
+    color:"black",
+  }*/
+/*type cartype={
+    price :0;
+    color : "not defined";
+}
+
+
+  class car {
+    price : number=0;
+    color: string='not defined'
+  }*/
+
+  //let carA :cartype = new car()
+
+
+ /* string : var chair=1;
+do{
+     var company :string = "masters";
+   var  color : string ="brown";
+   var result :string =company;
+   console.log(result),
+  chair++;
+}while(chair<=15);*/
+
+/*type chairtype={company:string,color:string,year:number,isUsed:boolean,material:string};
+var chair : chairtype[]=[{ company : "masters", color : "red",year : 2000 ,isUsed : true,material : "wood"
+
+}]  
+console.log(chair[0].company)*/
+  
+number : var i=0;
+type car={company:string,tyres:number,used:boolean,model:number,color:string};
+for(i=1;i<=10;i++){
+var tool: car[]=[
+{company :"honda", tyres:4,used : true,model :2023,color :"black"},
+{company :"tesla", tyres:5,used : false,model :2022,color :"red"},
+]
+console.log(tool[1].company)
+}
